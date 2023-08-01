376b18c09c36f3675713ddb8fa9e8abdd30822dd
우진 <woojin9812071207@gmail.com>
2023-08-01 15:31:28 +0900

    Fix: JwtProperties.java 수정 이전에 남아있던 값 제거

--- src/main/java/summer/mrplaylist/common/config/jwt/JwtProperties.java
+++ src/main/java/summer/mrplaylist/common/config/jwt/JwtProperties.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class JwtProperties {
     private String secretKey;
     private int accessTokenExpiration;
-    private int refreshTokenExpiration = 1000 * 3600 * 24 * 15; // 15일
+    private int refreshTokenExpiration;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 }
