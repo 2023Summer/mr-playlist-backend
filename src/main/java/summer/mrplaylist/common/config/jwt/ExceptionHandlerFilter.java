3762ae112b2c551f4f963789c9ddc138d5cacab6
Coodori_0616 <81786969+shinb-bong@users.noreply.github.com>
2023-08-03 09:09:15 +0900

    Feat: Control ErrorResponse (#34)
    에러 일괄 처리 (필터,spring)
    issue # 31

---
+++ src/main/java/summer/mrplaylist/common/config/jwt/ExceptionHandlerFilter.java
@@ -0,0 +1,41 @@
+package summer.mrplaylist.common.config.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import summer.mrplaylist.common.dto.ErrorResponse;
+import summer.mrplaylist.common.exception.TokenNotValidateException;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try{
+            filterChain.doFilter(request,response);
+        } catch (TokenNotValidateException e) {
+            setErrorResponse(HttpStatus.UNAUTHORIZED, response, e);
+        }
+    }
+
+    private void setErrorResponse(HttpStatus status, HttpServletResponse response, TokenNotValidateException e) throws IOException {
+        response.setStatus(status.value());
+        response.setContentType("application/json; charset=UTF-8");
+
+        response.getWriter().write(
+                new ErrorResponse(
+                        LocalDateTime.now(),
+                        e.getMessage()
+                )
+                        .convertToJson()
+        );
+    }
+}
+
---
+++ src/main/java/summer/mrplaylist/common/dto/ErrorResponse.java
@@ -0,0 +1,21 @@
+package summer.mrplaylist.common.dto;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private LocalDateTime timestamp;
+    private String message;
+
+    public String convertToJson() throws JsonProcessingException {
+        return objectMapper.writeValueAsString(this);
+    }
+
+}
