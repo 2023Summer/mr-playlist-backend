e3630ba27ce89ee94c4b1a154df7da33bbb8914e
coodori <daoh98@naver.com>
2023-08-09 11:56:51 +0900

    Feat: Add graceful shutdown
    issue #46

--- src/test/java/summer/mrplaylist/common/util/XlsxFileParserTest.java
+++ src/test/java/summer/mrplaylist/common/util/XlsxFileParserTest.java
@@ -5,26 +5,26 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
 import summer.mrplaylist.playlist.model.Playlist;
 import summer.mrplaylist.playlist.repository.PlaylistRepository;
 
 @SpringBootTest
 @Transactional
 public class XlsxFileParserTest {
-    @Autowired
-    private XlsxFileParser xlsxFileParser;
+	@Autowired
+	private XlsxFileParser xlsxFileParser;
 
-    @Autowired
-    private PlaylistRepository playlistRepository;
+	@Autowired
+	private PlaylistRepository playlistRepository;
 
+	@Test
+	void fileRead() throws Exception {
 
+		xlsxFileParser.fileRead();
+		Playlist playlist = playlistRepository.findById(1L)
+			.orElseThrow(() -> new IllegalArgumentException("not exist playlist"));
 
-    @Test
-    void fileRead() throws Exception {
-
-        xlsxFileParser.fileRead();
-        Playlist playlist = playlistRepository.findById(1L).orElseThrow(() -> new IllegalArgumentException("not exist playlist"));
-
-        Assertions.assertThat(playlist.getName()).isEqualTo("킹누의 노래 모음");
-    }
+		Assertions.assertThat(playlist.getName()).isEqualTo("킹누의 노래 모음");
+	}
 }
