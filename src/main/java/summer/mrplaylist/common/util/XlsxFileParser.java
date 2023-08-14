f08b189d743cce5fd93e05551d493cd538894c21
Coodori_0616 <81786969+shinb-bong@users.noreply.github.com>
2023-08-14 09:34:03 +0900

    Feat: Add Search Service (#53)
    * Feat: Add Search Service
    검색기능 추가
    issue #51
    
    * Docs: Edit PR template
    PR template 원복
    
    * Refactor: explicit join Music
    
    명시적 join
    issue #53
    
    * Refactor: Add fetchjoin
    
    * Refactor: refactor parameter
    
    * Feat: Add Cache ranking searchWord
    자주 검색한 단어를 보여주기위해 추가

--- src/main/java/summer/mrplaylist/common/util/XlsxFileParser.java
+++ src/main/java/summer/mrplaylist/common/util/XlsxFileParser.java
@@ -12,8 +12,6 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,11 +25,11 @@ import summer.mrplaylist.music.dto.MusicForm;
 import summer.mrplaylist.playlist.dto.PlaylistForm;
 import summer.mrplaylist.playlist.service.PlaylistService;
 
-@Profile({"test", "init"})
+// @Profile({"test", "init"})
 @Component
 @RequiredArgsConstructor
 @Slf4j
-@ConditionalOnResource(resources = "PlaylistData.xlsx")
+// @ConditionalOnResource(resources = "PlaylistData.xlsx")
 public class XlsxFileParser {
 
 	private final PlaylistService playlistService;
---
+++ src/main/java/summer/mrplaylist/playlist/repository/PlaylistQRepo.java
@@ -0,0 +1,73 @@
+package summer.mrplaylist.playlist.repository;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import summer.mrplaylist.playlist.model.Playlist;
+import summer.mrplaylist.playlist.model.QPlaylist;
+import summer.mrplaylist.search.dto.SearchCond;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class PlaylistQRepo {
+
+	private final JPAQueryFactory queryFactory;
+
+	QPlaylist qPlaylist = QPlaylist.playlist;
+
+	/**
+	 * 키워드 검색
+	 * 우선적으로 이름을 검색한뒤
+	 * 남은건 설명에서 검색한다.
+	 */
+	public Page<Playlist> findNameDescription(SearchCond cond, Pageable pageable) {
+		List<Playlist> result = findByName(cond, pageable);
+		int length = result.size();
+
+		if (pageable.getPageSize() > length) {
+			PageRequest page = PageRequest.of(0, pageable.getPageSize() - length);
+			List<Playlist> byDescription = findByDescription(cond, page, result);
+			result = Stream.of(result, byDescription)
+				.flatMap(x -> x.stream())
+				.toList();
+		}
+
+		return new PageImpl<>(result, pageable, result.size());
+	}
+
+	public List<Playlist> findByName(SearchCond cond, Pageable pageable) {
+		return queryFactory.selectFrom(qPlaylist)
+			.where(containName(cond.getWord()))
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+	}
+
+	public List<Playlist> findByDescription(SearchCond cond, Pageable pageable, List<Playlist> findList) {
+		return queryFactory.selectFrom(qPlaylist)
+			.where(containName(cond.getWord()),
+				qPlaylist.notIn(findList))
+			.limit(pageable.getPageSize())
+			.fetch();
+	}
+
+	private BooleanExpression containName(String word) {
+		return qPlaylist.name.contains(word);
+	}
+
+	private BooleanExpression containDescription(String word) {
+		return qPlaylist.description.contains(word);
+	}
+}
