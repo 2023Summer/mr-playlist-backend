b805b0f94481c8bde4a05f382c473ee83ecf20ab
우진 <woojin9812071207@gmail.com>
2023-08-21 15:40:56 +0900

    Refactor: Add SchedulingConfig @EnableScheduling Config 화

--- src/main/java/summer/mrplaylist/PlaylistApplication.java
+++ src/main/java/summer/mrplaylist/PlaylistApplication.java
@@ -3,13 +3,9 @@ package summer.mrplaylist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import summer.mrplaylist.common.config.jwt.JwtProperties;
 
-@EnableJpaAuditing
-@EnableScheduling
 @EnableConfigurationProperties(JwtProperties.class)
 @SpringBootApplication
 public class PlaylistApplication {
